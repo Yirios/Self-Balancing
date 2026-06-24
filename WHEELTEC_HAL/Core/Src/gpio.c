95cf02848105b12252bd5d94f2b9fa60fd06d8d4
yiriso <kszyc1001@163.com>
2026-06-24 21:05:25 +0800

    fix sampling bug

--- WHEELTEC_HAL/Core/Src/gpio.c
+++ WHEELTEC_HAL/Core/Src/gpio.c
@@ -68,7 +68,11 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = KEY2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+#if ACTUATOR_ID_MODE
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+#else
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+#endif
   HAL_GPIO_Init(KEY2_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
